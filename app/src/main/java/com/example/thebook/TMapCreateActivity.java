462159bd129fdf1be8c176854f86db25a0c30990
x19g025 <x19g025@mitsuhashi.ac.jp>
2021-12-01 10:40:13 +0900

    12/01

--- app/src/main/java/com/example/thebook/TMapCreateActivity.java
+++ app/src/main/java/com/example/thebook/TMapCreateActivity.java
@@ -3,6 +3,9 @@ package com.example.thebook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class TMapCreateActivity extends AppCompatActivity {
 
@@ -11,4 +14,5 @@ public class TMapCreateActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tmap_create);
     }
+
 }
